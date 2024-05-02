63f23e3b92b76e6b0971e6dec2185f98a6bb349e
Ka rusnanto <Karusnanto@gmail.com>
2024-05-02 19:46:12 +0700

    bismillah

---
+++ app/controllers/presensi.controller.js
@@ -0,0 +1,74 @@
+const db = require("../models");
+const Presensi = db.presensis;
+
+exports.index = (req, res) => {
+  Presensi.findAll()
+    .then((data) => {
+      res.json({
+        message: "Data presensi ditemukan",
+        data: data,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: err.message || "Data presensi tidak ditemukan",
+      });
+    });
+};
+
+exports.create = (req, res) => {
+  const presensi = {
+    nama: req.body.nama,
+    status: req.body.status,
+  };
+
+  Presensi.create(presensi)
+    .then((data) => {
+      res.json({
+        message: "Data presensi berhasil ditambahkan",
+        data: data,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: err.message || "Gagal ditambahkan",
+      });
+    });
+};
+
+exports.destroy = (req, res) => {
+  const id = req.params.id;
+  Presensi.destroy({
+    where: { id: id },
+  })
+    .then((data) => {
+        res.json({
+          message: "Data presensi berhasil dihapus",
+          data: data,
+        });
+    })  
+    .catch((err) => {
+      res.status(500).json({
+        message: err.message || "Data presensi gagal di hapus",
+      });
+    });
+};
+
+exports.update = (req, res) => {
+  const id = req.params.id; // Mengambil id dari permintaan HTTP
+
+  Presensi.update(req.body, {
+    where: { id: id }, // Menentukan kondisi untuk memperbarui data dengan id yang sesuai
+  })
+    .then((data) => {
+      res.json({
+        message: "Data presensi berhasil diupdate",
+        data: data,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: err.message || "Gagal diupdate",
+      });
+    });
+};
